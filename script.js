9c519b90fa7bca185350048d299e397ba838f0ee
AarishKod <58791118+AarishKod@users.noreply.github.com>
2024-02-12 10:19:11 -0500

    add files

---
+++ script.js
@@ -0,0 +1,29 @@
+function toggleMobileNavbar() {
+  let mobileBar = document.getElementById("float-bar");
+  let mobileNavBar = document.querySelector("nav.mobile-navbar");
+
+  if (mobileBar.style.display == "none") {
+    mobileBar.style.display = "block";
+    mobileNavBar.style.backgroundColor = "rgb(0, 0, 0)";
+  } else {
+    mobileBar.style.display = "none";
+    mobileNavBar.style.backgroundColor = "rgba(16, 24, 30, 0.8)";
+  }
+}
+
+document.createElement;
+
+// copy button on contact page
+
+let copyText = document.getElementById("copy-text");
+let copyButton = document.getElementById("copy-action");
+let toolTip = document.querySelector("em.copy-text-box");
+let toolTipText = toolTip.getAttribute("data-tooltip");
+
+copyButton.onclick = function () {
+  navigator.clipboard.writeText("aarishkod@gmail.com");
+  toolTip.setAttribute("data-tooltip", "Copied");
+  setTimeout(() => {
+    toolTip.setAttribute("data-tooltip", "Copy email");
+  }, 2000);
+};
